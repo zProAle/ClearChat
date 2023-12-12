3368f53da322211c6e9852ce1187154b6570eaf5
zProAle <zproale@gmail.com>
2023-12-12 11:01:05 +0100

    Update and fix script

--- clear.php
+++ clear.php
@@ -1,60 +1,37 @@
 <?php
-// check madaline
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
 include 'madeline.php';
 
-// start session
+// Your account session, follow this guide to get it https://docs.madelineproto.xyz/docs/LOGIN.html
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
-$MadelineProto->async(true);
-$MadelineProto->loop(function () use ($MadelineProto) {
-	
-	$times = 2629743; // time of the last message in second ex. 2629743 is a month
-	
-    yield $MadelineProto->start();
+$MadelineProto->start();
 
-    $me = yield $MadelineProto->getSelf();
+// Seconds to delay between each message
+$times = 2629743; //1 month
 
-    $MadelineProto->logger($me);
-	
-	$dialogs = yield $MadelineProto->getDialogs(); // get all chat
+/* Get all chat */
+$dialogs = $MadelineProto->getDialogs();
+foreach ($dialogs as $peer) {
+    if($peer['_'] === "peerUser"){ // check that it is not a channel or group
+        // Get last message in chat for check time
+        $messages_Messages = $MadelineProto->messages->getHistory(['peer' => $peer, 'offset_id' => 0, 'offset_date' => 0, 'add_offset' => 0, 'limit' => 1, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
+        $messages = $messages_Messages['messages'];
+        foreach(array_reverse($messages) as $i => $message){ 
+            $datacheck = time() - $message['date'];
+        }
 
-	foreach ($dialogs as $peer) {
-		try{
-			$messages = yield $MadelineProto->messages->getHistory([ // get last message from all chat
-				'peer' => $peer, 
-				'offset_id' => 0, 
-				'offset_date' => 0, 
-				'add_offset' => 0,
-				'limit' => 1,
-				'max_id' => 999, 
-				'min_id' => 0, 
-			]);
-			
-			$messages = $messages['messages'];
-			
-			foreach(array_reverse($messages) as $i => $message){ 
-				$datacheck = time() - $message['date'];
-				$type = $message['to_id']['_'];
-			}
-			if($type != "peerChannel"){ // check that it is not a channel
-				if($datacheck >= $times){ // check a ultimate messages
-					$test = yield $MadelineProto->messages->deleteHistory(['just_clear' => true, 'revoke' => true, 'peer' => $peer, 'max_id' => 99999,]);
-					echo "I delete chat! \n ";
-					sleep(2);
-				} else {
-					echo "This chat is recent I don't delete it! \n";
-					sleep(2);
-				}
-			} else {
-				echo "Channel detected, i skip this! \n";
-				sleep(1);
-			}
-		} catch (Exception $e) {
-			echo 'Exception error: ',  $e->getMessage(), "\n";
-		}
-	}
-
-});
-?>
+        // Check if the last message is older than the time set
+        if($datacheck >= $times){
+            $delete = $MadelineProto->messages->deleteHistory(peer: $peer['user_id'], just_clear: true, revoke: true);
+            $MadelineProto->logger("Deleted chat: ".$peer['user_id']);
+            sleep(1);
+        } else {
+            $MadelineProto->logger("Not deleted chat because it is not old enough: ".$peer['user_id']);
+        }
+    } else {
+        $MadelineProto->logger("Not deleted channel or group");
+    }
+}
+$MadelineProto->echo('OK, done!');
